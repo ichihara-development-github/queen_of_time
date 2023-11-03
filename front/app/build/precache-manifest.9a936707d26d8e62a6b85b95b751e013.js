6a9cf89f5832834e5c64a2d9d8683b309c8a26c2
yoshihiroichihara <yoshihiroichihara@macbookpro.local>
2023-11-04 00:19:09 +0900

    move_env

---
+++ front/app/build/precache-manifest.9a936707d26d8e62a6b85b95b751e013.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "32baca9d71cff32ca3cd",
+    "url": "/static/css/main.58ee5a35.chunk.css"
+  },
+  {
+    "revision": "32baca9d71cff32ca3cd",
+    "url": "/static/js/main.2d38dca6.chunk.js"
+  },
+  {
+    "revision": "16c53ef586c275db0250",
+    "url": "/static/js/runtime~main.19f9c24d.js"
+  },
+  {
+    "revision": "5a2b20403656838dcec8",
+    "url": "/static/css/2.41470115.chunk.css"
+  },
+  {
+    "revision": "5a2b20403656838dcec8",
+    "url": "/static/js/2.4e574e87.chunk.js"
+  },
+  {
+    "revision": "837506aba4d7fe12653c",
+    "url": "/static/js/3.5b58e830.chunk.js"
+  },
+  {
+    "revision": "e22677e7e3bd64301ec815f0876312f3",
+    "url": "/static/media/employee-default.e22677e7.jpg"
+  },
+  {
+    "revision": "d78a09ee39d90527dc437a7cedeaae6e",
+    "url": "/static/media/icon.d78a09ee.png"
+  },
+  {
+    "revision": "71506fe69668f9cb09069f330569941a",
+    "url": "/static/media/mountain.71506fe6.jpg"
+  },
+  {
+    "revision": "39957d64945fb917e75355835616d535",
+    "url": "/static/media/offices-buildings-svgrepo-com.39957d64.svg"
+  },
+  {
+    "revision": "7e269903ddc3fbcae657d9227cae10a6",
+    "url": "/static/media/desc_image1.7e269903.jpg"
+  },
+  {
+    "revision": "bb044e3e988ef7404d0418e4f0908310",
+    "url": "/static/media/desc_image2.bb044e3e.jpg"
+  },
+  {
+    "revision": "426a67141d6da081df969bec6ec22acf",
+    "url": "/static/media/employees.426a6714.png"
+  },
+  {
+    "revision": "47766b226aa415eb600d6ebdade7a8e1",
+    "url": "/static/media/shift.47766b22.png"
+  },
+  {
+    "revision": "0fb1df28fa739d4623d6502aad9fd57b",
+    "url": "/static/media/manage.0fb1df28.png"
+  },
+  {
+    "revision": "f033fa2984bdfe3b32b00fe22453452c",
+    "url": "/static/media/timestamp.f033fa29.png"
+  },
+  {
+    "revision": "d181c0c02141d2e1de0f1b0335da5927",
+    "url": "/static/media/calendar.d181c0c0.png"
+  },
+  {
+    "revision": "a5c9cd2331b7dad3c669bef2393efe8c",
+    "url": "/static/media/chat.a5c9cd23.png"
+  },
+  {
+    "revision": "b4ba5882ce4b1da3af87036fc7c64045",
+    "url": "/index.html"
+  }
+];
